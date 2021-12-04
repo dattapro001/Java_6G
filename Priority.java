313ee799e849a36ee693c51ba61e34171dce5fd3
Chinmoy Datta Priom <92098651+dattapro001@users.noreply.github.com>
2021-12-05 00:52:34 +0600

    Add files via upload

---
+++ Priority.java
@@ -0,0 +1,20 @@
+// Thread Priority
+public class Priority implements Runnable {
+	public void run() {
+		System.out.println((Thread.currentThread().getName()));
+	}
+
+	public static void main(String[] args) {
+		Priority pro = new Priority();
+        Thread t1 = new Thread(pro,"Frist Thread");
+        Thread t2 = new Thread(pro,"Second Thread");
+        Thread t3 = new Thread(pro,"Third Thread");
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.MAX_PRIORITY);
+        t3.setPriority(Thread.NORM_PRIORITY);
+        t1.start();
+        t2.start();
+        t3.start();
+	}
+
+}
